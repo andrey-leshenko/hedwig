31e708fdf07e2997acf3b01845fbc7dca5bab0f4
Shir Amir <shirkaluka@gmail.com>
2016-08-08 17:47:20 +0300

    Begin calculating drone transform

--- stabilize/stabilize.cpp
+++ stabilize/stabilize.cpp
@@ -13,12 +13,40 @@ using cv::Size;
 using cv::Point2f;
 using cv::Point3f;
 using cv::FileStorage;
+using cv::Affine3f;
 using cv::Affine3d;
+using cv::SVD;
+using cv::Scalar;
+using cv::Vec3f;
 
 const vector<int> cameraIndexes{1, 2};
 const vector<const char*> cameraCalib{"cam3011.yaml", "cam7192.yaml"};
-const Size chessboardSize{8, 5}; 
-const float chessSquareSize = 2.9;
+const Size chessboardSize{5, 4};
+const float chessSquareSize = 3.025;
+
+Point3f pointsMean(const vector<Point3f>& points)
+{
+	Point3f sum;
+	for (Point3f p : points) {
+		sum.x += p.x;
+		sum.y += p.y;
+		sum.z += p.z;
+	}
+	int count = points.size();
+	return Point3f{sum.x / count, sum.y / count, sum.z / count};
+}
+
+Mat createPointMatrix(const vector<Point3f>& points)
+{
+	Mat m{static_cast<int>(points.size()), 3, CV_32F};
+
+	for (int i = 0; i < points.size(); i++) {
+		m.at<float>(i,1) = points[i].x;
+		m.at<float>(i,2) = points[i].y;
+		m.at<float>(i,3) = points[i].z;
+	}
+	return m;
+}
 
 int main()
 {
@@ -61,9 +89,11 @@ int main()
 			}
 		}
 	}
-	
+
+	//////// Read camera matrices from xml/yaml file ////////
+
 	vector<Mat> cameraMatrixes(cameraCount);
-	
+
 	{
 		for (int i = 0; i < cameraCount; i++) {
 			FileStorage fs(cameraCalib[i], FileStorage::READ);
@@ -75,6 +105,8 @@ int main()
 		}
 	}
 
+	//////// Calculate the chessboard's initial position ////////
+
 	vector<Point3f> objectPoints;
 
 	for (int z = 0; z < chessboardSize.height; z++)	{
@@ -82,12 +114,19 @@ int main()
 			objectPoints.push_back(Point3f{x * chessSquareSize, 0, z * chessSquareSize});
 		}
 	}
-	
+
+	Point3f initialPosition = pointsMean(objectPoints);
+	Mat initialPoints = createPointMatrix(objectPoints);
+
+	//////// Calculate Projection matrices for each camera ////////
+
 	vector<vector<Point2f>> imagePoints(cameraCount);
 	vector<Mat> rvecs(cameraCount);
 	vector<Mat> tvecs(cameraCount);
 	vector<Affine3d> cameraTransforms(cameraCount);
 	vector<Mat> projectionMatrixes(cameraCount);
+
+	int pre = cv::getTickCount();
 
 	{
 		for (int i = 0; i < frames.size(); i++) {
@@ -97,39 +136,38 @@ int main()
 		for (int i = 0; i < frames.size(); i++) {
 			cameras[i].retrieve(frames[i]);
 		}
-		
+
 		for (int i = 0; i < frames.size(); i++) {
 			bool found = cv::findChessboardCorners(
 				frames[i],
-				chessboardSize, 
-				imagePoints[i], 
+				chessboardSize,
+				imagePoints[i],
 				CV_CALIB_CB_ADAPTIVE_THRESH | CV_CALIB_CB_NORMALIZE_IMAGE | CV_CALIB_CB_FAST_CHECK);
-				
+
 			if(!found) {
 				std::cerr << "Chessboard corners were not found." << std::endl;
 				return 1;
-			}			
+			}
 		}
 
 		for (int i = 0; i < frames.size(); i++) {
-			bool found = cv::solvePnP(objectPoints, 
-			imagePoints[i], 
-			cameraMatrixes[i], 
-			Mat{}, 
-			rvecs[i], 
+			bool found = cv::solvePnP(objectPoints,
+			imagePoints[i],
+			cameraMatrixes[i],
+			Mat{},
+			rvecs[i],
 			tvecs[i]);
-			
+
 			if (!found) {
 				std::cerr << "Couldn't calibrate camera." << std::endl;
 				return 1;
-			}	
+			}
 		}
 
 		for (int i = 0; i < frames.size(); i++) {
 			cameraTransforms[i] = Affine3d(rvecs[i], tvecs[i]);
 			Mat rtMatrix = Mat{cameraTransforms[i].matrix}.rowRange(0, 3);
 			projectionMatrixes[i] = cameraMatrixes[i] * rtMatrix;
-
 			cameraTransforms[i] = cameraTransforms[i].inv();
 		}
 	}
@@ -146,7 +184,10 @@ int main()
 		window.setWidgetPose(name, cameraTransforms[i]);
 	}
 
+	//////// Triangulate and visualize the outputted data ////////
+
 	window.showWidget("axes", cv::viz::WCoordinateSystem{20});
+	window.showWidget("drone", cv::viz::WCube{Point3f{-20, -5, -20}, Point3f{20, 5, 20}});
 	window.showWidget("calibrationChessboard", cv::viz::WCloud{objectPoints, cv::viz::Color::red()});
 
 	do {
@@ -177,8 +218,37 @@ int main()
 
 			vector<Point3f> triangulated;
 			cv::convertPointsFromHomogeneous(homogeneous.t(), triangulated);
+
+			Point3f currentPosition = pointsMean(triangulated);
+
+			Mat currPoints = createPointMatrix(triangulated);
+			Affine3f currTransform;
+
+			{
+				Mat covarianceMatrix{initialPoints.t() * currPoints};
+				Mat lsf, rsft, sv;
+
+				SVD::compute(covarianceMatrix, sv, lsf, rsft);
+				double det = cv::determinant(rsft.t() * lsf.t());
+				int d = 1;
+				if(det < 0)
+					d = -1;
+				if(det == 0)
+					d = 0;
+				Mat diagonal = Mat::eye(3, 3, CV_32F);
+				diagonal.at<float>(3,3) = d;
+				Mat u = rsft.t() * diagonal * lsf.t();
+
+				currTransform.rotation(u);
+				currTransform.translation(Vec3f{currentPosition} - Vec3f{initialPosition});
+			}
+
 			window.showWidget("chessboard", cv::viz::WCloud{triangulated});
 			window.setRenderingProperty("chessboard", cv::viz::POINT_SIZE, 4);
+			window.setWidgetPose("drone", currTransform);
+
+			int post = cv::getTickCount();
+			//std::cout << (post - pre)/cv::getTickFrequency() << std::endl;
 		}
 
 		window.spinOnce(1, true);
